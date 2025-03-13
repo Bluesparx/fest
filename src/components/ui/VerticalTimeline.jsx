69d0a59ada1928da9a8956876fcf9cce1e4c6e2d
Riddhi Rustagi <riddhi147btcse22@igdtuw.ac.in>
2025-03-13 21:36:21 +0530

    minor change

--- src/components/ui/VerticalTimeline.jsx
+++ src/components/ui/VerticalTimeline.jsx
@@ -5,7 +5,7 @@ const VerticalTimeline = ({ events }) => {
     <div className="flex flex-col items-center space-y-8">
       {events.map((event, index) => (
         <div key={index} className="flex flex-col items-center text-center">
-          <div className="w-2 h-2 bg-yellow-500 rounded-full mb-2"></div>
+       
           <div className="border-l-4 border-yellow-500 pl-4">
             <p className="text-lg font-bold text-white">{event.time}</p>
             <h3 className="text-2xl text-yellow-500 font-semibold">{event.title}</h3>

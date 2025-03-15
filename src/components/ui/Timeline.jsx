f1195a6ec8792888ff47ac1d19d1ae4fdaead7f8
Riddhi Rustagi <riddhi147btcse22@igdtuw.ac.in>
2025-03-15 19:36:05 +0530

    small changes

--- src/components/ui/Timeline.jsx
+++ src/components/ui/Timeline.jsx
@@ -58,8 +58,8 @@ export default function HorizontalTimeline({ events }) {
 
                     {/* Overlay Text (Centered on Image) */}
                     <div className="absolute inset-0 bg-black/50 flex flex-col justify-center items-center text-white text-center rounded-xl p-4">
-                      <h2 className="text-lg md:text-xl font-bold">{event.title}</h2>
-                      <p className="text-sm md:text-base mt-1">{event.description}</p>
+                      <h2 className="text-lg md:text-xl font-bold mx-4">{event.title}</h2>
+                      <p className="text-sm md:text-base mt-1 mx-4">{event.description}</p>
                     </div>
                   </div>
 
